6cf7ea56323aa9df6abcb18ae558e40727901e7e
Seb1776 <sebastianwho1776@gmail.com>
2022-06-24 00:00:01 -0500

    Update 0-making_change.py

--- 0x19-making_change/0-making_change.py
+++ 0x19-making_change/0-making_change.py
@@ -7,26 +7,26 @@ meet a given amount total.
 
 
 def makeChange(coins, total):
-	"""
-	Return: fewest number of coins needed to meet total.
-	-If total is 0 or less, return 0.
-	-If total cannot be met by any number
-	"""
-	if total <= 0:
-		return 0
+    """
+    Return: fewest number of coins needed to meet total.
+    -If total is 0 or less, return 0.
+    -If total cannot be met by any number
+    """
+    if total <= 0:
+        return 0
 
-	coins.sort(reverse=True)
+    coins.sort(reverse=True)
 
-	i, ncoins = (0, 0)
-	cpy_total = total
-	len_coins = len(coins)
+    i, ncoins = (0, 0)
+    cpy_total = total
+    len_coins = len(coins)
 
-	while(i < len_coins and cpy_total > 0):
-		if (cpy_total - coins[i]) >= 0:
-			cpy_total -= coins[i]
-			ncoins += 1
-		else:
-			i += 1
+    while(i < len_coins and cpy_total > 0):
+        if (cpy_total - coins[i]) >= 0:
+            cpy_total -= coins[i]
+            ncoins += 1
+        else:
+            i += 1
 
-	check = cpy_total > 0 and ncoins > 0
-	return -1 if check or ncoins == 0 else ncoins
+    check = cpy_total > 0 and ncoins > 0
+    return -1 if check or ncoins == 0 else ncoins
